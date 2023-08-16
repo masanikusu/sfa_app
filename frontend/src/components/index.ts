23a116f649022d9a60c6721bc3f10ec3f3d30e21
masanix <71020514+masanikusu@users.noreply.github.com>
2023-08-16 23:39:23 +0900

    add

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as NavBar } from './navbar/Navbar';
-export { default as MenuBar } from './menubar/MenuBar';
 export { default as Footer } from './footer/Footer';
+// export { default as MenuBar } from './menubar/MenuBar';
